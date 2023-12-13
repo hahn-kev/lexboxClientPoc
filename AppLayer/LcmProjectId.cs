af06febb40733d8fb150045d222a49f0e0a0cd6f
Kevin Hahn <hahn.kev@gmail.com>
2023-12-13 12:42:22 +0700

    first attempt at loading lcm projects

---
+++ AppLayer/LcmProjectId.cs
@@ -0,0 +1,43 @@
+﻿using SIL.LCModel;
+
+namespace AppLayer;
+
+public record LcmDirectories(string ProjectsDirectory, string TemplateDirectory) : ILcmDirectories;
+
+class LcmProjectIdentifier : IProjectIdentifier
+{
+    public LcmProjectIdentifier(ILcmDirectories LcmDirs, string database)
+    {
+        LcmDirectories = LcmDirs;
+        Path = GetPathToDatabase(LcmDirs, database);
+    }
+
+    private static string GetPathToDatabase(ILcmDirectories LcmDirs, string database)
+    {
+        return System.IO.Path.Combine(LcmDirs.ProjectsDirectory,
+            database,
+            database + LcmFileHelper.ksFwDataXmlFileExtension);
+    }
+
+    public ILcmDirectories LcmDirectories { get; private set; }
+
+    public bool IsLocal => true;
+
+    public string Path { get; set; }
+
+    public string ProjectFolder => System.IO.Path.GetDirectoryName(Path);
+
+    public string SharedProjectFolder => ProjectFolder;
+
+    public string ServerName => null;
+
+    public string Handle => Name;
+
+    public string PipeHandle => throw new NotSupportedException();
+
+    public string Name => System.IO.Path.GetFileNameWithoutExtension(Path);
+
+    public BackendProviderType Type => BackendProviderType.kXML;
+
+    public string UiName => Name;
+}
---
+++ AppLayer/ProjectLoader.cs
@@ -0,0 +1,49 @@
+﻿using SIL.LCModel;
+using StructureMap;
+
+namespace AppLayer;
+
+public class ProjectLoader
+{
+    private const string ProjectFolder = "/Projects";
+    private const string TemplatesFolder = "/Templates";
+
+    static ProjectLoader()
+    {
+        Directory.CreateDirectory(ProjectFolder);
+        Directory.CreateDirectory(TemplatesFolder);
+    }
+
+    public static async Task<FlexProject> LoadProject(Stream projectFile, string fileName)
+    {
+        fileName = Path.GetFileName(fileName);
+        var projectFilePath = Path.Combine(ProjectFolder, Path.GetFileNameWithoutExtension(fileName), fileName);
+        Directory.CreateDirectory(Path.GetDirectoryName(projectFilePath));
+        var localFileCopy = File.Create(projectFilePath);
+        await projectFile.CopyToAsync(localFileCopy);
+        localFileCopy.Flush();
+        var lcmDirectories = new LcmDirectories(ProjectFolder, TemplatesFolder);
+        var progress = new LcmThreadedProgress();
+        var cache = LcmCache.CreateCacheFromExistingData(
+            new LcmProjectIdentifier(lcmDirectories, Path.GetFileNameWithoutExtension(fileName)),
+            null,
+            new LfLcmUi(progress.SynchronizeInvoke),
+            lcmDirectories,
+            new LcmSettings(),
+            progress
+        );
+        Console.WriteLine("Done creating cache");
+        var entries = cache.ServiceLocator.GetInstance<IRepository<ILexEntry>>()
+            .AllInstances()
+            .Select(e => new Entry(e.LexemeFormOA.Form.VernacularDefaultWritingSystem.Text));
+        Console.WriteLine("Done loading project");
+        return new FlexProject(entries.ToArray());
+    }
+}
+
+public class FlexProject(Entry[] entries)
+{
+    public Entry[] Entries { get; init; } = entries;
+}
+
+public record Entry(string LexemeForm);
