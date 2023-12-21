792742b6d646ccc4dab4c7dde39955d65ad74a90
Kevin Hahn <hahn.kev@gmail.com>
2023-12-21 16:06:09 +0700

    correct mistake in example sentence changing a reference to a string

--- lexboxClientContracts/Entry.cs
+++ lexboxClientContracts/Entry.cs
@@ -3,29 +3,29 @@
 public class Entry
 {
     public Guid Id { get; set; }
-    public MultiString LexemeForm { get; set; }
-    public MultiString CitationForm { get; set; }
-    public MultiString LiteralMeaning { get; set; }
-    public List<Sense> Senses { get; set; }
-    public MultiString Note { get; set; }
+    public MultiString LexemeForm { get; set; } = new();
+    public MultiString CitationForm { get; set; } = new();
+    public MultiString LiteralMeaning { get; set; } = new();
+    public List<Sense> Senses { get; set; } = [];
+    public MultiString Note { get; set; } = new();
 }
 
 public class Sense
 {
     public Guid Id { get; set; }
-    public MultiString Definition { get; set; }
-    public MultiString Gloss { get; set; }
-    public string PartOfSpeech { get; set; }
-    public List<string> SemanticDomain { get; set; }
-    public List<ExampleSentence> ExampleSentences { get; set; }
+    public MultiString Definition { get; set; } = new();
+    public MultiString Gloss { get; set; } = new();
+    public string PartOfSpeech { get; set; } = string.Empty;
+    public List<string> SemanticDomain { get; set; } = [];
+    public List<ExampleSentence> ExampleSentences { get; set; } = [];
 }
 
 public class ExampleSentence
 {
     public Guid Id { get; set; }
-    public MultiString Sentence { get; set; }
-    public MultiString Translation { get; set; }
-    public MultiString Reference { get; set; }
+    public MultiString Sentence { get; set; } = new();
+    public MultiString Translation { get; set; } = new();
+    public string Reference { get; set; } = string.Empty;
 }
 
 /// <summary>
